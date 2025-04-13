567def219e63085c7947fcf215cd4f9db8ca437b
Hernan-Erices <alejandroarevaloerices@gmail.com>
2025-04-13 18:24:14 -0400

    portafolio sin finalizar

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function pdfCV() {
+    window.open("assets/pdf/Alejandro_Arevalo_CV.pdf");
+}
+function github() {
+    window.open('https://github.com/Hernan-Erices');
+}
+function linkedin() {
+    window.open('https://www.linkedin.com/in/alejandro-hernán-arévalo-erices-13a0a6230');
+}
